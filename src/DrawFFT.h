904ed9dce82901f75ab68a7a95f71cb83a3a7bad
Ben Marsan <biimiimii@gmail.com>
2018-06-10 15:15:26 -0500

    Reformat comments

--- src/DrawFFT.h
+++ src/DrawFFT.h
@@ -10,7 +10,7 @@ class DrawFFT
   private:
     Adafruit_IS31FL3731_Wing matrix;
 
-    uint8_t peak[15] = {},     // Peak level of each column for falling dots
+    uint8_t peak[15] = {},      // Peak level of each column for falling dots
         dotCount = 0,           // Frame counter for delaying falling dots
         colCount = 0,           // Frame counter for storing past column data
         drawFrame = 1;          // Frame counter for double bufferred drawing
@@ -21,26 +21,26 @@ class DrawFFT
             LED_HIGH = 64,
             LED_ON = 255;
 
-    uint8_t col[15][10];                // Column levels for past 10 frames
-    int16_t minLvlAvg[15] = {};    // Dynamic adjustment of high and low
-    int16_t maxLvlAvg[15] = {};        //   levels of graph (probably only sets first item?)
-    int16_t colDiv[15] = {};           // Used to filter FFT to 15 columns
+    uint8_t col[15][10];        // Column levels for past 10 frames
+    int16_t minLvlAvg[15] = {}; // Dynamic adjustment of high and low
+    int16_t maxLvlAvg[15] = {}; //   levels of graph (probably only sets first item?)
+    int16_t colDiv[15] = {};    // Used to filter FFT to 15 columns
 
     // Noise constants to subtract from FFT output
     static constexpr int16_t noise[32] = {
-            190, 105, 8, 8, 5, 5, 5, 5, // 0-7
-            5, 5, 5, 5, 5, 5, 5, 5, // 8-15
-            3, 3, 3, 2, 2, 2, 2, 2, // 16-23
-            2, 2, 2, 2, 2, 2, 2, 2  // 24-32
+            190, 105, 8, 8, 5, 5, 5, 5,     // 0-7
+            5, 5, 5, 5, 5, 5, 5, 5,         // 8-15
+            3, 3, 3, 2, 2, 2, 2, 2,         // 16-23
+            2, 2, 2, 2, 2, 2, 2, 2          // 24-32
         };
 
     // FFT outputs are multiplied by EQ constants to change weight of certain
     // bins.
     static constexpr float eq[32] = {
-            1, 1, 1, 1, 1, 1, 1, 1, // 0-7
-            1, 1, 1, 1, 1, 1, 1, 1, // 8-15
-            1, 1, 1, 1, 1, 1, 1, 1, // 16-23
-            1, 1, 1, 1, 1, 1, 1, 1,  // 24-32
+            1, 1, 1, 1, 1, 1, 1, 1,         // 0-7
+            1, 1, 1, 1, 1, 1, 1, 1,         // 8-15
+            1, 1, 1, 1, 1, 1, 1, 1,         // 16-23
+            1, 1, 1, 1, 1, 1, 1, 1,         // 24-32
         };
 
   public:
